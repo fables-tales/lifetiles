a9a6001e2c374f2ca9175baee84ff01672f56403
Sam Phippen <samphippen@googlemail.com>
2012-08-31 21:49:49 +0100

    And suddenly rails
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifetiles::Application.config.secret_token = 'a29859d0b217e2a43b35243601f5f7cb536f67b9f3bb10084e96d186489767294f5d9c4e0529225c3f8b1f94b658f18541764b40d0eb6c37a28db0c2cabf8e02'
