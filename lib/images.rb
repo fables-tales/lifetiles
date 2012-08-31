2d4c67a864f3501bc21e731ab50877fd30c4777f
Sam Phippen <samphippen@googlemail.com>
2012-08-31 22:27:39 +0100

    Add some image getting code
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ lib/images.rb
@@ -0,0 +1,19 @@
+def get_twitter_profile(username)
+  url = Twitter.user(username)[:profile_image_url].gsub("_normal", "")
+  extension = url[-4..-1]
+  filename = "#{Random.rand(1000000000)}#{extension}"
+
+  result = Http.get url
+
+  handle = open("public/images/#{filename}", "wb")
+  handle.write(result)
+  handle.close()
+
+  return filename
+end
+
+def resize_image(path)
+  img = (Magick::Image.read path)[0]
+  thumb = img.resize_to_fill(200, 200)
+  thumb.write path
+end
