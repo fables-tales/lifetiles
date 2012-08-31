9fe4fb388489b04c5b07e457053f154b755c768c
Sam Phippen <samphippen@googlemail.com>
2012-08-31 23:05:28 +0100

    Add a seed to add my twitter profile picture
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+require "images"
+
+path = ImageManager.twitter_profile("@fnzcuvccra")
+ImageManager.resize_image("public/images/#{path}")
+md5 = ImageManager.get_md5("public/images/#{path}")
+
+
+if Tile.where("image_md5 = ?", md5).length == 0
+  t = Tile.new()
+  t.description = "Twitter Profile Picture"
+  t.image = "images/#{path}"
+  t.image_md5 = md5
+  t.save
+end
