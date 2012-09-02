33d1a22257c713fb2262dffc748f3e762b1bcae1
Sam Phippen <samphippen@googlemail.com>
2012-09-02 14:53:16 +0100

    Refactor twitter tile generator some what
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/twitter_images.rb
+++ lib/twitter_images.rb
@@ -1,27 +1,31 @@
 require "images"
+
+class Twitter::Tweet
+  def has_media?
+    not self[:media].empty?
+  end
+end
+
 class TwitterTileGenerator
+  def self.tile_from_tweet(tweet)
+      media = tweet.media.first
+      url = media.attrs[:media_url]
+      ext = url[-4..-1]
+      name = "#{Random.rand(10000000)}#{ext}"
+      path = "public/images/#{name}"
+      ImageManager.get_image(url, path)
+      ImageManager.resize_image(path)
+      md5 = ImageManager.get_md5(path)
+      if Tile.where("image_md5 = ?", md5).empty?
+        Tile.manufacture(tweet.text, "images/#{name}", tweet[:created_at])
+      end
+  end
   def self.make_tiles(user)
     tweets = Twitter.user_timeline(user, options = {:include_entities => true,
                                                     :count => 200})
-    tweets.each do |t|
-      if t.media != []
-        media = t.media.first
-        url = media.attrs[:media_url]
-        ext = url[-4..-1]
-        name = "#{Random.rand(10000000)}#{ext}"
-        path = "public/images/#{name}"
-        ImageManager.get_image(url, path)
-        ImageManager.resize_image(path)
-        md5 = ImageManager.get_md5(path)
-        if Tile.where("image_md5 = ?", md5).length == 0
-          tile = Tile.new
-          tile.description = t.text
-          tile.image = "images/#{name}"
-          tile.image_md5 = ImageManager.get_md5(path)
-          tile.save
-          tile.created_at = t[:created_at]
-          tile.save
-        end
+    tweets.each do |tweet|
+      if tweet.has_media?
+        self.tile_from_tweet tweet
       end
     end
   end
