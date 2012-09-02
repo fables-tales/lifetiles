ab027364e5914b1cc90d5f66df7f9d51a4ca4410
Sam Phippen <samphippen@googlemail.com>
2012-09-02 14:52:31 +0100

    Add a manufacture method to the tile class
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,2 +1,11 @@
 class Tile < ActiveRecord::Base
+  def self.manufacture(description, image, creation_date=nil)
+    tile = Tile.new
+    tile.description = description
+    tile.image = image
+    tile.image_md5 = ImageManager.get_md5("public/#{image}")
+    tile.save
+    tile.created_at = creation_date unless creation_date = nil
+    tile.save
+  end
 end
