bcbe6e4c0773ca7e9709b3fec29a37359b88c5a9
Sam Phippen <samphippen@googlemail.com>
2012-09-02 17:37:13 +0100

    Beat these specs very hard
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- spec/lib/twitter_images_spec.rb
+++ spec/lib/twitter_images_spec.rb
@@ -1,6 +1,15 @@
-require "twitter_images"
 require "spec_helper"
+require "twitter_images.rb"
 
+
+def file_md5(path)
+  Digest::MD5.hexdigest(File.read(path))
+end
+
+SHARED_MESSAGES = {
+                    :text => "test tweet",
+                    :[] => [:created_at => DateTime.now]
+                  }
 describe TwitterTileGenerator do
   describe TwitterTileGenerator, "#tile_from_tweet" do
 
@@ -11,37 +20,107 @@ describe TwitterTileGenerator do
 
       stub(:media, media_messages)
     end
-    let (:target) do
-      messages = {
-        :media => [media],
-        :text  => "test text",
-        :[] => [:created_at => DateTime.now]
+
+    let (:geo) do
+      geo_messages = {
+        :coordinates => [51, -1]
       }
-      stub(:target, messages)
+
+      stub(:geo, geo_messages)
     end
 
-    subject do
-      TwitterTileGenerator.tile_from_tweet(target)
-      Tile.all
+
+    shared_examples_for "a tile from a tweet" do
+      it "only creates one tile" do
+        tiles.length.should == 1
+      end
+
+      it "should create a tile with the right description" do
+        tiles.first.description.should == target.text
+      end
+
+      it "should create a tile with a 200x200 image" do
+        path = "public/#{tiles.first.image}"
+        (Magick::Image.read path)[0].columns.should == 200
+        (Magick::Image.read path)[0].rows.should == 200
+      end
+
+      it "should have the right md5" do
+        path = "public/#{tiles.first.image}"
+        tiles.first.image_md5.should == file_md5(path)
+      end
     end
 
-    it "only creates one tile" do
-      subject.length.should == 1
+
+    context "tweet with no media or geo" do
+      let (:target) do
+        messages = SHARED_MESSAGES.clone
+        messages[:has_media?] = false
+
+        stub(:target, messages)
+      end
+
+      subject do
+        TwitterTileGenerator.tile_from_tweet(target)
+        Tile.all
+      end
+
+      it "should not create a tile" do
+        subject.empty?.should == true
+      end
+
     end
 
-    it "should create a tile with the right description" do
-      subject.first.description.should == target.text
+    context "no geo" do
+      let (:target) do
+        messages = SHARED_MESSAGES.clone
+        messages[:geo] = nil
+        messages[:has_media?] = true
+        messages[:media] = [media]
+
+        stub(:target, messages)
+      end
+
+      subject do
+        TwitterTileGenerator.tile_from_tweet(target)
+        Tile.all
+      end
+
+      it_should_behave_like "a tile from a tweet" do
+        let(:tiles) { subject }
+      end
+
+      it "should have nil geo information" do
+        subject.first.lat.should  == nil
+        subject.first.long.should == nil
+      end
+
     end
 
-    it "should create a tile with a 200x200 image" do
-      path = "public/#{subject.first.image}"
-      (Magick::Image.read path)[0].columns.should == 200
-      (Magick::Image.read path)[0].rows.should == 200
-    end
+    context "full tweet" do
 
-    it "should have the right md5" do
-      path = "public/#{subject.first.image}"
-      subject.first.image_md5.should == Digest::MD5.hexdigest(File.read(path))
+      let (:target) do
+        messages = SHARED_MESSAGES.clone
+        messages[:geo] = geo
+        messages[:has_media?] = true
+        messages[:media] = [media]
+
+        stub(:target, messages)
+      end
+
+      subject do
+        TwitterTileGenerator.tile_from_tweet(target)
+        Tile.all
+      end
+
+      it_should_behave_like "a tile from a tweet" do
+        let (:tiles) { subject }
+      end
+
+      it "should have the right location" do
+        subject.first.lat.should  == geo.coordinates[0]
+        subject.first.long.should == geo.coordinates[1]
+      end
     end
   end
 end
