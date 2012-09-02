c0907099a57760e982ebd43e1deeb4357eb0b694
Sam Phippen <samphippen@googlemail.com>
2012-09-02 17:36:10 +0100

    Refactor twitter_images heavyhandedly
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/twitter_images.rb
+++ lib/twitter_images.rb
@@ -5,33 +5,33 @@ class Twitter::Tweet
 end
 
 class TwitterTileGenerator
+
+  def self.handle_geo(tweet, tile)
+    if tweet.geo != nil
+      tile.lat = tweet.geo.coordinates[0]
+      tile.long = tweet.geo.coordinates[1]
+      tile.save
+    end
+  end
+
   def self.tile_from_tweet(tweet)
-      media = tweet.media.first
-      url = media.attrs[:media_url]
-      ext = url[-4..-1]
-      name = "#{Random.rand(10000000)}#{ext}"
-      path = "public/images/#{name}"
-      ImageManager.get_image(url, path)
-      ImageManager.resize_image(path)
-      md5 = ImageManager.get_md5(path)
-      if Tile.where("image_md5 = ?", md5).empty?
-        tile = Tile.manufacture(tweet.text, "images/#{name}", tweet[:created_at])
-        if tweet.geo != nil
-          puts "geo"
-          tile.lat = tweet.geo.coordinates[0]
-          tile.long = tweet.geo.coordinates[1]
-          tile.save
-        end
-      end
+    return unless tweet.has_media?
+    media = tweet.media.first
+    url = media.attrs[:media_url]
+    name = ImageManager.acquire(url)
+    path = "public/images/#{name}"
+    md5 = ImageManager.get_md5(path)
+    if Tile.where("image_md5 = ?", md5).empty?
+      tile = Tile.manufacture(tweet.text, "images/#{name}", tweet[:created_at])
+      self.handle_geo(tweet, tile)
+    end
   end
 
   def self.make_tiles(user)
     tweets = Twitter.user_timeline(user, options = {:include_entities => true,
                                                     :count => 200})
     tweets.each do |tweet|
-      if tweet.has_media?
-        self.tile_from_tweet tweet
-      end
+      self.tile_from_tweet tweet
     end
   end
 end
