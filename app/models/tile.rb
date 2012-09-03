8e9da500870535ab868cb51552531bbcd671f057
Sam Phippen <samphippen@googlemail.com>
2012-09-03 16:20:41 +0100

    Make tiles strip urls when being saved
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -10,12 +10,21 @@ class Tile < ActiveRecord::Base
   # Returns the tile it has created
   def self.manufacture(description, image, creation_date=nil)
     tile = Tile.new
-    tile.description = description
+    tile.description = remove_uris(description)
     tile.image = image
     tile.image_md5 = ImageManager.get_md5("public/#{image}")
     tile.save
     tile.created_at = creation_date unless creation_date == nil
     tile.save
     return tile
+  end
+  URI_REGEX = %r"((?:(?:[^ :/?#]+):)(?://(?:[^ /?#]*))(?:[^ ?#]*)(?:\?(?:[^ #]*))?(?:#(?:[^ ]*))?)"
+
+  def self.remove_uris(text)
+    text.split(URI_REGEX).collect do |s|
+      unless s =~ URI_REGEX
+        s
+      end
+    end.join
   end
 end
